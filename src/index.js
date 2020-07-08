aba0a517c9abfd6155a95ff114500da91876c336
Ricardo <ricardo.rendich@gmail.com>
2020-07-08 01:34:43 -0400

    CORS fixed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const engine = require('ejs-mate')
 const path = require('path')
+const cors = require('cors')
 
 // inicializations
 const app = express();
+//app.use(cors()) // Simple Usage (Enable All CORS Requests)
+
 
 // setup
 app.engine('ejs', engine);
@@ -20,13 +23,22 @@ app.use( express.static(path.join(__dirname+ '/public')) );
 // start server
 const PORT = process.env.PORT || 3000;
 
+
+var whitelist = ['http://localhost', 'http://leafletnodejs.herokuapp.com/', 'https://leafletnodejs.herokuapp.com/']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+ 
+app.get('/', cors(corsOptions), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for a whitelisted domain.'})
+})
+
 app.listen(PORT, () => {
     console.log("Server on port " + PORT);
 });
-// https://www.youtube.com/watch?v=Zy89Nj7tNNM
-// COMANDS:
-//npm init --yes
-//npm i express ejs-mate socket.io
-//node src/index.js
-// npm i nodemon -D
-// npm run dev
